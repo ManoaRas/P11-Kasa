9acef7c1fff27889ce9e28a6225ba33682b6a085
Manoa Rasolomalala <manoatony.pro@gmail.com>
2024-07-22 18:13:03 +0200

    Adding scss + Fixing router index

--- src/index.jsx
+++ src/index.jsx
@@ -5,12 +5,12 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 
-import Home from "./pages/Home.jsx"
+import Home from "./pages/Home"
 // import Accommodation from "./pages/Accommodation"
 // import About from "./pages/About"
 // import Error from "./pages/Error"
 
-import './styles/index.css'
+import "./styles/main.scss"
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
