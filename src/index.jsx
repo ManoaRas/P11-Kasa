9f2131107d49f433cde231e85755ec01ab711327
Manoa Rasolomalala <manoatony.pro@gmail.com>
2024-07-22 15:45:56 +0200

    Setup Kasa app

---
+++ src/index.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+import Header from "./components/Header"
+
+import Home from "./pages/Home.jsx"
+// import Accommodation from "./pages/Accommodation"
+// import About from "./pages/About"
+// import Error from "./pages/Error"
+
+import './styles/index.css'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <React.StrictMode>
+    <Router>
+      <Header />
+
+      <main>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          {/* <Route path='/accommodation/:id' element={<Accommodation />} /> */}
+          {/* <Route path='/about' element={<About />} /> */}
+          {/* <Route path='*' element={<Error />} /> */}
+        </Routes>
+      </main>
+
+      {/* <Footer /> */}
+    </Router>
+  </React.StrictMode>
+)
